cc24ed9447d2244434e7405bf6db95a848b0c54e
Mahmoud <nagboumahmoud22@gmail.com>
2024-08-31 16:42:58 +0100

    final

--- static/js/index.js
+++ static/js/index.js
@@ -1,38 +1,10 @@
-document.getElementById('predictForm').addEventListener('submit', async function (event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-    const data = {};
-
-    formData.forEach((value, key) => {
-        data[key] = value;
+document.querySelectorAll('.img-c').forEach(img => {
+    img.addEventListener('click', function () {
+        document.getElementById('popupImage').src = this.src;
+        document.getElementById('imagePopup').style.display = 'block';
     });
-
-    try {
-        const response = await fetch('/predict', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        });
-        const result = await response.json();
-
-        Swal.fire({
-            title: result.prediction === 1 ? 'High Risk Detected' : 'Low Risk Detected',
-            text: `Probability of having heart disease: ${(result.probability[1] * 100).toFixed(2)}%.`,
-            icon: result.prediction === 1 ? 'warning' : 'success',
-            confirmButtonText: 'OK',
-            customClass: {
-                popup: 'colored-popup',
-                title: 'custom-title'
-            }
-        });
-    } catch (error) {
-        console.error('Error:', error);
-        Swal.fire({
-            title: 'Error',
-            text: `An error occurred: ${error.message}`,
-            icon: 'error',
-            confirmButtonText: 'OK'
-        });
-    }
 });
+
+function closePopup() {
+    document.getElementById('imagePopup').style.display = 'none';
+}
